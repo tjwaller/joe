4277159aa9a91ff85771167eb80ffe6d8d3dddc2
Alex Addington <axaddington@uga.edu>
2024-03-21 16:10:52 -0400

    made server work, run by "npm run dev"

--- frontend/server.js
+++ frontend/server.js
@@ -1,15 +1,10 @@
 const express = require('express');
-const webpack = require('webpack');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const webpackConfig = require('./webpack.config.js');
-
 const app = express();
-const compiler = webpack(webpackConfig);
 
-app.use(webpackDevMiddleware(compiler, {
-    publicPath: webpackConfig.output.publicPath,
-}));
+app.get('/api/joe', (req, res) => {
+    res.send('Hello World!');
+});
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!\n');
+app.listen(3001, () => {
+    console.log('Server is running on port 3000');
 });
